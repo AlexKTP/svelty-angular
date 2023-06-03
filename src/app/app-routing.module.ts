013845d636f7b712c6dd475fd77cfd310dc2dea6
AlexKTP <alexandre.malachin@gmail.com>
2023-06-03 16:48:36 +0200

    issue #12 404 not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './components/home/home.component';
 import { CreateformComponent } from './components/createform/createform.component';
 import { LoginComponent } from './components/login/login.component';
 import { canActivate, canActivateChild } from './utils/permission';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'form', canActivate: [canActivate], component: CreateformComponent },
-  { path: 'home', canActivate: [canActivate], component: HomeComponent }
+  { path: 'home', canActivate: [canActivate], component: HomeComponent },
+  { path: '**', canActivate: [canActivate], component: NotFoundComponent },
   // Add more routes for other pages if needed
 ];
 
