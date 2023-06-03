df0d44b31566746598d2ef9eac51689addc003d9
AlexKTP <alexandre.malachin@gmail.com>
2023-06-03 11:57:04 +0200

    issue #3 add new record component

--- src/app/components/createform/createform.component.ts
+++ src/app/components/createform/createform.component.ts
@@ -1,5 +1,9 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ITrack } from 'src/app/models/track.interface';
+import { TrackService } from 'src/app/services/tracks/track.service';
+import { LoggerService } from 'src/app/services/logger/logger.service';
 
 @Component({
   selector: 'app-createform',
@@ -11,9 +15,58 @@ export class CreateformComponent implements OnInit {
   firstDayOfTheTear: string | null | undefined;
   lastDayOfTheTear: string | null | undefined;
 
-  constructor(private datePipe: DatePipe) { }
+  registerForm!: FormGroup;
+
+
+  constructor(private builder: FormBuilder, private datePipe: DatePipe, private trackService: TrackService, private logger: LoggerService) { }
 
   ngOnInit() {
+    this.initForm()
+    this.initDate()
+  }
+
+  registerFormSubmit() {
+    if (this.registerForm.get('weight')?.value == null || this.registerForm.get('weight')?.value < 0) {
+    }
+    const track: ITrack = {
+      weight: this.registerForm.get('weight')?.value,
+      chest: this.registerForm.get('chest')?.value,
+      abs: this.registerForm.get('abs')?.value,
+      hip: this.registerForm.get('hip')?.value,
+      bottom: this.registerForm.get('bottom')?.value,
+      leg: this.registerForm.get('thigh')?.value,
+      date: this.registerForm.get('date')?.value,
+      toSynchronize: true,
+      userId: 17
+    }
+
+    this.trackService.addNewTrack(track).subscribe({
+      next: (nextValue) => {
+        this.logger.info(CreateformComponent.name + ' New track Added!')
+      },
+      error: (error) => {
+        this.logger.error(CreateformComponent + ' ' + error.message + ' ' + error.status)
+      },
+      complete: () => {
+        // Handle completion
+      }
+
+    })
+  }
+
+  initForm() {
+    this.registerForm = this.builder.group({
+      weight: [null, Validators.required],
+      chest: [null],
+      abs: [null],
+      hip: [null],
+      bottom: [null],
+      thigh: [null],
+      date: [this.currentDate]
+    }, {});
+  }
+
+  initDate() {
     this.currentDate = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
     if (this.currentDate != null && this.currentDate != undefined) {
       var begin = new Date();
